149bb66f57584e3676e8099704e35247b62ba0d5
siddhantp <siddhant@greenlightplanet.com>
2019-12-31 13:24:19 +0530

    Structure Commited for Market Pulse Demo

---
+++ app/src/main/java/com/marketpulse/scans/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.marketpulse.scans
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.marketpulse.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
